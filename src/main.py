c139f17929c50a1498c46136b0296e0e0b8f70c2
B-Johansen <148041726+B-Johansen@users.noreply.github.com>
2023-10-16 20:55:26 +0200

    Updates to main.py
    Running as intended, now with I/O at choice 4.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import sys
-import subprocess
+import tkinter as tk
+from tkinter import messagebox, simpledialog
 from credentials import api_key  # Import the api_key from credentials.py
 import requests
 import base64
-
 
 # Install requests from PyPi
 def install(package):
@@ -32,7 +31,7 @@ class IPaddress:
     def get_ip_report(self):
         var_urlIP = self.var_vtip + self.var_ip
         response = requests.get(var_urlIP, headers=self.headers)
-        print(response.text)
+        return response.text  # return the response text as a string
 
 
 class Domain:
@@ -48,7 +47,7 @@ class Domain:
     def get_domain_report(self):
         var_urlDomain = self.var_vtdomain + self.var_domain
         response = requests.get(var_urlDomain, headers=self.headers)
-        print(response.text)
+        return response.text
 
 
 class File:
@@ -64,8 +63,7 @@ class File:
     def get_file_report(self):
         var_urlFile = self.var_vtfile + self.var_file
         response = requests.get(var_urlFile, headers=self.headers)
-        print(response.text)
-
+        return response.text
 
 class Url:
     var_vturl = "https://www.virustotal.com/api/v3/urls/"
@@ -78,18 +76,16 @@ class Url:
         self.var_url = var_url
 
     def check_for_identifier(self):
-        identifier_input = input("Do you have an identifier? (yes/no): ")
-        if identifier_input.lower() == "no":
-            self.var_url = self.generate_url_id(input("Enter URL: "))  # Generate URL ID here
-            print(f"New URL identifier generated: {self.var_url}")
+        identifier_input = messagebox.askquestion("Identifier", "Do you have an identifier?")
+        if identifier_input == "no":
+            url = simpledialog.askstring("Input", "Enter URL:")
+            self.var_url = self.generate_url_id(url)  # Generate URL ID here
+            messagebox.showinfo("Information", f"New URL identifier generated: {self.var_url}")
             return False
-        elif identifier_input.lower() == "yes":
-            self.var_url = input("Enter URL identifier: ")
-            print("User has an identifier. Continue with the program.")
+        elif identifier_input == "yes":
+            self.var_url = simpledialog.askstring("Input", "Enter URL identifier:")
+            messagebox.showinfo("Information", "User has an identifier. Continue with the program.")
             return True
-        else:
-            print("Invalid input. Please enter 'yes' or 'no'.")
-            return False
 
     def generate_url_id(self, url):
         return base64.urlsafe_b64encode(url.encode()).decode().strip("=")
@@ -97,27 +93,54 @@ class Url:
     def get_url_report(self):
         var_urlUrl = self.var_vturl + self.var_url
         response = requests.get(var_urlUrl, headers=self.headers)
-        print(response.text)
+        return response.text
 
+def run_program():
+    output.delete(1.0, tk.END)  # Clear the output field before running the program
+    choice = variable.get()
 
-if __name__ == "__main__":
-    choice = input(
-        "Enter '1' for IP address, '2' for Domain, '3' for File, or '4' for URL: "
-    )
+    if choice == "IP address":
+        ip_addr = simpledialog.askstring("Input", "Enter IP-address:")
+        ip_addr_instance = IPaddress(var_ip=ip_addr)
+        result = ip_addr_instance.get_ip_report()
+        output.insert(tk.END, result)
 
-    if choice == "1":
-        ip_addr_instance = IPaddress(var_ip=input("Enter IP-address: "))
-        ip_addr_instance.get_ip_report()
+    elif choice == "Domain":
+        domain = simpledialog.askstring("Input", "Enter Domain:")
+        domain_instance = Domain(var_domain=domain)
+        result = domain_instance.get_domain_report()
+        output.insert(tk.END, result)
 
-    elif choice == "2":
-        domain_instance = Domain(var_domain=input("Enter Domain: "))
-        domain_instance.get_domain_report()
+    elif choice == "File":
+        file_hash = simpledialog.askstring("Input", "Enter File Hash:")
+        file_instance = File(var_file=file_hash)
+        result = file_instance.get_file_report()
+        output.insert(tk.END, result)
 
-    elif choice == "3":
-        file_instance = File(var_file=input("Enter File Hash: "))
-        file_instance.get_file_report()
-
-    elif choice == "4":
+    elif choice == "URL":
         url_instance = Url()
         url_instance.check_for_identifier()
-        url_instance.get_url_report()
+        result = url_instance.get_url_report()
+        output.insert(tk.END, result)
+
+
+root = tk.Tk()
+root.geometry("500x400")
+
+label = tk.Label(root, text="Select an option and run the program")
+label.pack()
+
+options = ['IP address', 'Domain', 'File', 'URL']
+variable = tk.StringVar(root)
+variable.set(options[0])
+
+dropdown = tk.OptionMenu(root, variable, *options)
+dropdown.pack()
+
+run_button = tk.Button(root, text="Run Program", command=run_program)
+run_button.pack()
+
+output = tk.Text(root, height=10, width=50)
+output.pack()
+
+root.mainloop()
