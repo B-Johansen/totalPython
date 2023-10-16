ec3c2b140c5c260b0901e5944d897fec730a2450
B-Johansen <148041726+B-Johansen@users.noreply.github.com>
2023-10-16 09:15:38 +0200

    Initial commit
    Initial commit of local files.

---
+++ src/UrlID.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import base64
+
+class UrlId:
+    def __init__(self, url):
+        self.url = url
+        self.url_id = self.generate_url_id()
+
+    def generate_url_id(self):
+        return base64.urlsafe_b64encode(self.url.encode()).decode().strip("=")
+
+    def print_details(self):
+        print(f"URL: {self.url}, URL ID: {self.url_id}")
+
+url = input("Enter URL: ")
+url_obj = UrlId(url)
+url_obj.print_details()
---
+++ src/main.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+from credentials import api_key  # Import the api_key from credentials.py
+import requests
+import UrlID  # Assuming UrlID is defined somewhere in your code
+
+
+# Install requests from PyPi
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+# Check if requests is installed, if not, install it
+try:
+    import requests
+except ImportError:
+    install("requests")
+
+
+class IPaddress:
+    var_vtip = "https://www.virustotal.com/api/v3/ip_addresses/"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": api_key,
+    }  # Use the imported api_key
+
+    def __init__(self, var_ip):
+        self.var_ip = var_ip
+
+    def get_ip_report(self):
+        var_urlIP = self.var_vtip + self.var_ip
+        response = requests.get(var_urlIP, headers=self.headers)
+        print(response.text)
+
+
+class Domain:
+    var_vtdomain = "https://www.virustotal.com/api/v3/domains/"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": api_key,
+    }  # Use the imported api_key
+
+    def __init__(self, var_domain):
+        self.var_domain = var_domain
+
+    def get_domain_report(self):
+        var_urlDomain = self.var_vtdomain + self.var_domain
+        response = requests.get(var_urlDomain, headers=self.headers)
+        print(response.text)
+
+
+class File:
+    var_vtfile = "https://www.virustotal.com/api/v3/files/"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": api_key,
+    }  # Use the imported api_key
+
+    def __init__(self, var_file):
+        self.var_file = var_file
+
+    def get_file_report(self):
+        var_urlFile = self.var_vtfile + self.var_file
+        response = requests.get(var_urlFile, headers=self.headers)
+        print(response.text)
+
+
+class Url:
+    var_vturl = "https://www.virustotal.com/api/v3/urls/"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": api_key,
+    }  # Use the imported api_key
+
+    def __init__(self, var_url):
+        self.var_url = var_url
+
+    def check_for_identifier(self):
+        identifier = input("Do you have an identifier? (yes/no): ")
+        if identifier.lower() == "no":
+            UrlID.generate_url_id()  # Assuming generate_url_id() is a method in UrlID
+            return False
+        elif identifier.lower() == "yes":
+            print("User has an identifier. Continue with the program.")
+            return True
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+            return False
+
+    def get_url_report(self):
+        var_urlUrl = self.var_vturl + self.var_url
+        response = requests.get(var_urlUrl, headers=self.headers)
+        print(response.text)
+
+
+# Ask for IP address when creating the instance and call the method
+if __name__ == "__main__":
+    
+    choice = input(
+        "Enter '1' for IP address, '2' for Domain, '3' for File, or '4' for URL: "
+    )
+    
+    if choice == '1':
+        
+        ip_addr_instance = IPaddress(var_ip=input("Enter IP-address: "))
+        ip_addr_instance.get_ip_report()
+        
+    elif choice == '2':
+        
+        domain_instance = Domain(var_domain=input("Enter Domain: "))
+        domain_instance.get_domain_report()
+        
+    elif choice == '3':
+        
+        file_instance = File(var_file=input("Enter File Hash: "))
+        file_instance.get_file_report()
+        
+    elif choice == '4':
+        
+        url_instance = Url(var_url=input("Enter URL identifier: "))
+        
+        if url_instance.check_for_identifier():
+            url_instance.get_url_report()
